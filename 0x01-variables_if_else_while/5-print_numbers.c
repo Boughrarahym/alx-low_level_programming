762ba5729e4fb6d8c147834286b32bb7f9c0400c
Boughrarahym <bgrrhind@gmail.com>
2023-03-17 10:15:21 -0700

    prints all single digit numbers of base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base ten
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int h;
+
+	for (h = 0; h < 10; h++)
+		printf("%d", h);
+
+	printf("\n");
+	return (0);
+}
