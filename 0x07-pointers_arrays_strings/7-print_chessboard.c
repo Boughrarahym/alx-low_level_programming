8765179e5508496975ce1ee98abee4586ea4a324
Boughrarahym <bgrrhind@gmail.com>
2023-04-03 14:34:57 -0700

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,27 +1,21 @@
 #include "main.h"
 
 /**
- * print_chessboard - function that prints the chessboard
- * @a:  array of chars
- * Return: void
- */
-
+  *print_chessboard - function that prints the chessboard
+  *@a: pointer
+  *
+  *Return: void.
+  */
 void print_chessboard(char (*a)[8])
 {
-	int hym, bgrr;
+	int  hym, bgrr;
 
-	for (hym = 0; hym < 8; hym++)
+	for (hym = 0; hym <= 7; hym++)
 	{
-		for (bgrr = 0; bgrr < 8; bgrr++)
-	{
-		if (bgrr == 7)
-	_putchar(a[hym][bgrr]);
-		else
-	{
+		for (bgrr = 0; bgrr <= 7; bgrr++)
+		{
 			_putchar(a[hym][bgrr]);
-			_putchar(' ');
-	}
-	}
-	_putchar('\n');
+		}
+		_putchar(10);
 	}
 }
