8bc31ff02fe5e8d8c064dacdcbb71e878100bf41
Boughrarahym <bgrrhind@gmail.com>
2023-04-19 08:37:57 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be prited
+ * @f: pointer to the function
+ * Boughrara Hym
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+		f(name);
+}
