50813c227533b5fb2f40465433b7b0f199fb00cb
Boughrarahym <bgrrhind@gmail.com>
2023-04-20 06:34:46 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct printer - new struct type defining a printer
+ * @symbol: symbol representing a data type
+ * @print: a function pointer to a function that prints
+ *         a data type corresponding to symbole
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
 #endif
