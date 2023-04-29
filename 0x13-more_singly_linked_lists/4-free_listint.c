9aafceb71af6463465b9c92145eda0182a6ff18b
Boughrarahym <bgrrhind@gmail.com>
2023-04-29 10:53:12 -0700

    Linked lists teach you the importance of pointers and the horrors of null pointers

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that  Frees a listint_t list
+ * @head: pointer to the head
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *t;
+
+	while (head)
+	{
+		t = head->next;
+		free(head);
+		head = t;
+	}
+}
